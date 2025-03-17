86b95e60006044245157674a559e43cd693aca68
Yan <134351027+Yanmaiscedo@users.noreply.github.com>
2025-03-16 22:18:57 -0300

    Feat: enum eDir

--- PingPong.cpp
+++ PingPong.cpp
@@ -5,6 +5,22 @@
 
 using namespace std;
 
+enum eDir
+{
+    STOP = 0,
+    LEFT = 1,
+    UPLEFT = 2,
+    DOWNLEFT = 3,
+    RIGHT = 4,
+    UPRIGHT = 5,
+    DOWNRIGHT = 6
+};
+
+class cBall
+{
+
+};
+
 int main(){
 
     return 0;
